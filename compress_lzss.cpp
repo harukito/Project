f619a3393555bc82cf3b0e5509c68fa988068bac
Harukito Kurayami <dispku@gmail.com>
2019-01-28 21:32:13 -0300

    Update compress_lzss.cpp

--- compress_lzss.cpp
+++ compress_lzss.cpp
@@ -1,106 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define EI 11  /* typically 10..13 */
-#define EJ  4  /* typically 4..5 */
-#define P   1  /* If match length <= P then output one character */
-#define N (1 << EI)  /* buffer size */
-#define F ((1 << EJ) + P)  /* lookahead buffer size */
+#define EI 11
+#define EJ  4
+#define P   1
+#define N (1<<EI)
+#define F ((1<<EJ)+P)
 
-int bit_buffer = 0, bit_mask = 128;
-unsigned long codecount = 0, textcount = 0;
-unsigned char buffer[N * 2];
+int bit_buffer=0, bit_mask=128;
+unsigned long codecount=0, textcount=0;
+unsigned char buffer[N*2];
 FILE *infile, *outfile;
 
-void error(void)
-{
-    printf("Output error\n");  exit(1);
+void error(void){
+    printf("Output error\n"); exit(1);
 }
 
-void putbit1(void)
-{
-    bit_buffer |= bit_mask;
-    if ((bit_mask >>= 1) == 0) {
-        if (fputc(bit_buffer, outfile) == EOF) error();
-        bit_buffer = 0;  bit_mask = 128;  codecount++;
+void putbit1(void){
+    bit_buffer|=bit_mask;
+    if ((bit_mask>>=1)==0){
+        if (fputc(bit_buffer, outfile)==EOF) error();
+        bit_buffer=0; bit_mask=128; codecount++;
     }
 }
 
-void putbit0(void)
-{
-    if ((bit_mask >>= 1) == 0) {
-        if (fputc(bit_buffer, outfile) == EOF) error();
-        bit_buffer = 0;  bit_mask = 128;  codecount++;
+void putbit0(void){
+    if ((bit_mask>>=1)==0){
+        if (fputc(bit_buffer, outfile)==EOF) error();
+        bit_buffer=0; bit_mask=128; codecount++;
     }
 }
 
-void flush_bit_buffer(void)
-{
-    if (bit_mask != 128) {
-        if (fputc(bit_buffer, outfile) == EOF) error();
+void flush_bit_buffer(void){
+    if (bit_mask!=128) {
+        if (fputc(bit_buffer, outfile)==EOF) error();
         codecount++;
     }
 }
 
-void output1(int c)
-{
+void output1(int c){
     int mask;
-    
     putbit1();
-    mask = 256;
-    while (mask >>= 1) {
+    mask=256;
+    while (mask>>=1) {
         if (c & mask) putbit1();
         else putbit0();
     }
 }
 
-void output2(int x, int y)
-{
+void output2(int x, int y){
     int mask;
-    
     putbit0();
     mask = N;
-    while (mask >>= 1) {
+    while (mask >>= 1){
         if (x & mask) putbit1();
         else putbit0();
     }
     mask = (1 << EJ);
-    while (mask >>= 1) {
+    while (mask >>= 1){
         if (y & mask) putbit1();
         else putbit0();
     }
 }
 
-void encode(void)
-{
+void encode(void){
     int i, j, f1, x, y, r, s, bufferend, c;
-    
-    for (i = 0; i < N - F; i++) buffer[i] = ' ';
-    for (i = N - F; i < N * 2; i++) {
-        if ((c = fgetc(infile)) == EOF) break;
-        buffer[i] = c;  textcount++;
+    for (i =0; i<N-F; i++) buffer[i]=' ';
+    for (i =N-F; i<N*2; i++){
+        if ((c=fgetc(infile))==EOF) break;
+        buffer[i]=c;  textcount++;
     }
-    bufferend = i;  r = N - F;  s = 0;
+    bufferend=i;  r=N-F;  s=0;
     while (r < bufferend) {
-        f1 = (F <= bufferend - r) ? F : bufferend - r;
-        x = 0;  y = 1;  c = buffer[r];
-        for (i = r - 1; i >= s; i--)
-            if (buffer[i] == c) {
-                for (j = 1; j < f1; j++)
-                    if (buffer[i + j] != buffer[r + j]) break;
-                if (j > y) {
-                    x = i;  y = j;
+        f1=(F<=bufferend-r)?F:bufferend-r;
+        x=0; y=1; c=buffer[r];
+        for (i=r-1; i>=s; i--)
+            if (buffer[i]==c) {
+                for (j=1; j<f1; j++)
+                    if (buffer[i+j]!=buffer[r+j])
+                    break;
+                if (j>y){
+                    x=i;  y=j;
                 }
             }
-        if (y <= P) output1(c);
-        else output2(x & (N - 1), y - 2);
-        r += y;  s += y;
-        if (r >= N * 2 - F) {
-            for (i = 0; i < N; i++) buffer[i] = buffer[i + N];
-            bufferend -= N;  r -= N;  s -= N;
-            while (bufferend < N * 2) {
-                if ((c = fgetc(infile)) == EOF) break;
-                buffer[bufferend++] = c;  textcount++;
+        if (y<=P) output1(c);
+        else output2(x&(N-1), y-2);
+        r+=y;  s+=y;
+        if (r>=N*2-F) {
+            for (i=0; i<N; i++) buffer[i]=buffer[i+N];
+            bufferend-=N; r-=N; s-=N;
+            while (bufferend<N*2) {
+                if ((c=fgetc(infile))==EOF)
+                break;
+                buffer[bufferend++]=c;  textcount++;
             }
         }
     }
@@ -143,9 +135,9 @@ void decode(void)
             if ((i = getbit(EI)) == EOF) break;
             if ((j = getbit(EJ)) == EOF) break;
             for (k = 0; k <= j + 1; k++) {
-                c = buffer[(i + k) & (N - 1)];
+                c = buffer[(i+k) & (N - 1)];
                 fputc(c, outfile);
-                buffer[r++] = c;  r &= (N - 1);
+                buffer[r++]=c; r&=(N-1);
             }
         }
     }
@@ -156,21 +148,21 @@ int main(int argc, char *argv[])
     int enc;
     char *s;
     
-    if (argc != 4) {
+    if (argc!=4) {
         printf("Usage: lzss e/d infile outfile\n\te = encode\td = decode\n");
         return 1;
     }
-    s = argv[1];
-    if (s[1] == 0 && (*s == 'd' || *s == 'D' || *s == 'e' || *s == 'E'))
-        enc = (*s == 'e' || *s == 'E');
+    s=argv[1];
+    if (s[1]==0 && (*s=='d'||*s=='D'||*s=='e'||*s=='E'))
+        enc=(*s=='e'||*s=='E');
     else {
-        printf("? %s\n", s);  return 1;
+        printf("? %s\n", s);
     }
-    if ((infile  = fopen(argv[2], "rb")) == NULL) {
-        printf("? %s\n", argv[2]);  return 1;
+    if ((infile  = fopen(argv[2], "rb"))==NULL) {
+        printf("? %s\n", argv[2]);
     }
-    if ((outfile = fopen(argv[3], "wb")) == NULL) {
-        printf("? %s\n", argv[3]);  return 1;
+    if ((outfile = fopen(argv[3], "wb"))==NULL) {
+        printf("? %s\n", argv[3]);
     }
     if (enc) encode();  else decode();
     fclose(infile);  fclose(outfile);
