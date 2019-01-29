cb52c0916f0db5ffe4adbae92a160ad0ef4e8f34
Harukito Kurayami <dispku@gmail.com>
2019-01-28 23:31:28 -0300

    Update compress_lzw.cpp

--- compress_lzw.cpp
+++ compress_lzw.cpp
@@ -1,17 +1,15 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
 #include <string>
 #include <map>
- 
-// Compress a string to a list of output symbols.
-// The result will be written to the output iterator
-// starting at "result"; the final iterator is returned.
+
 template <typename Iterator>
 Iterator compress(const std::string &uncompressed, Iterator result) {
-  // Build the dictionary.
   int dictSize = 256;
   std::map<std::string,int> dictionary;
   for (int i = 0; i < 256; i++)
     dictionary[std::string(1, i)] = i;
- 
   std::string w;
   for (std::string::const_iterator it = uncompressed.begin();
        it != uncompressed.end(); ++it) {
@@ -21,23 +19,16 @@ Iterator compress(const std::string &uncompressed, Iterator result) {
       w = wc;
     else {
       *result++ = dictionary[w];
-      // Add wc to the dictionary.
       dictionary[wc] = dictSize++;
       w = std::string(1, c);
     }
   }
- 
-  // Output the code for w.
   if (!w.empty())
     *result++ = dictionary[w];
   return result;
 }
- 
-// Decompress a list of output ks to a string.
-// "begin" and "end" must form a valid range of ints
 template <typename Iterator>
 std::string decompress(Iterator begin, Iterator end) {
-  // Build the dictionary.
   int dictSize = 256;
   std::map<int,std::string> dictionary;
   for (int i = 0; i < 256; i++)
@@ -46,7 +37,7 @@ std::string decompress(Iterator begin, Iterator end) {
   std::string w(1, *begin++);
   std::string result = w;
   std::string entry;
-  for ( ; begin != end; begin++) {
+  for ( ;begin!=end;begin++){
     int k = *begin;
     if (dictionary.count(k))
       entry = dictionary[k];
@@ -54,10 +45,7 @@ std::string decompress(Iterator begin, Iterator end) {
       entry = w + w[0];
     else
       throw "Bad compressed k";
- 
     result += entry;
- 
-    // Add w+entry[0] to the dictionary.
     dictionary[dictSize++] = w + entry[0];
  
     w = entry;
@@ -65,17 +53,11 @@ std::string decompress(Iterator begin, Iterator end) {
   return result;
 }
  
-#include <iostream>
-#include <iterator>
-#include <vector>
- 
-int main() {
+int main(){
   std::vector<int> compressed;
   compress("TOBEORNOTTOBEORTOBEORNOT", std::back_inserter(compressed));
   copy(compressed.begin(), compressed.end(), std::ostream_iterator<int>(std::cout, ", "));
   std::cout << std::endl;
   std::string decompressed = decompress(compressed.begin(), compressed.end());
   std::cout << decompressed << std::endl;
- 
-  return 0;
 }
